14f33ed669edbc0778c7e9afb6b8b6915a9a1b09
henriel03 <39703654+henriel03@users.noreply.github.com>
2018-06-11 11:51:23 -0300

    Update muitos_objetos_vetors.js

--- muitos_objetos_vetors.js
+++ muitos_objetos_vetors.js
@@ -1,50 +1,103 @@
-var vx = []; 
-var vy = [];
-var vdx = []
-var vdy = []
-var vtam = [];
-var qt = 30; 
-var vcorR = []; 
-var vcorG = [];
-var vcorB = [];
-var tamArea; 
-
+var disparo= false;
+var a
+var b
+var x = 15;
+var y = 250;
+var yq = 100;
+var vidas = 3;
+var pontos = 0;
+var raio = 20;
+var raioJ = 15;
+var corAmarelo = true; 
+var colisao = false; 
 function setup() {
-  frameRate(30);
-  tamArea = 600; 
-  createCanvas(tamArea,tamArea);
-  for ( i = 0; i < qt; i++) { 
-    vx[i] = random(0,tamArea); 
-    vy[i] = random(0,tamArea);
-    vtam[i] = random(5,15);
-    vdx[i] = random(-6,6);
-    vdy[i] = random(-6,6);
-    vcorR[i] = random(0,255);
-    vcorG[i] = random(0,255);
-    vcorB[i] = random(0,255);
-  }
-  
+  createCanvas(500, 500);
+  x=15;
+  a=x;
+  y=250;
+  b=y;
+  //xInimigo = ellipse; 
+ //yInimigo = ellipse;
+  //dx = 8
+ //dy = 10
 }
 
 function draw() {
-  background(0); 
+  background(0);
+   if ( dist(a,b,460,yq) < raio+raioJ ) {
+     if ( colisao == false) { 
+       corAmarelo = ! corAmarelo;      
+       colisao = true; 
+     }
+ }
+ else {
+    colisao = false;  
+} //colisão
   
-  for ( i = 0; i < qt; i++) {
-    fill(vcorR[i], vcorG[i], vcorB[i]);
-    vx[i] = vx[i] + vdx[i];
-    vy[i] = vy[i] + vdy[i]; 
-    if ( vx[i] > width || vx[i] < 0 ) {
-       vdx[i] = -vdx[i]; 
-       vcorR[i] = random(0,255);
-       vcorG[i] = random(0,255);
-       vcorB[i] = random(0,255);
-    }
-    if ( vy[i] > height || vy[i] < 0 ) {
-       vdy[i] = -vdy[i]; 
-      
-    }
+ 
+ //ellipse(xInimigo, yInimigo, 1*raio, 1*raio); 
+  
+ 
+ 
+ fill(230,230,0);
+ //ellipse(a,b,raioJ,raioJ); 
+  
+   textSize(25);
+  fill(250,400,10);
+  text("Vidas: "+vidas, 5, 60);
+  text("Pontos: "+pontos, 5, 90);
+  if (keyIsDown(CONTROL) && (! disparo) ){ 
+    disparo = true; 
+    a=x;
+    b=y;
     
-    ellipse(vx[i],vy[i],vtam[i],vtam[i]); 
+  }
+  if (disparo) {
+    a=a+19;
+    if (a> width) {
+       disparo = false; 
+    }
+      ellipse(a,b,1*raio,1*raio); //isso é o desenho do disparo
+
   }
   
+  if (keyIsDown(LEFT_ARROW))
+    x-=5;
+
+  if (keyIsDown(RIGHT_ARROW))
+    x+=5;
+
+  if (keyIsDown(UP_ARROW))
+    y-=5;
+
+  if (keyIsDown(DOWN_ARROW))
+    y+=5;
+if(y<25){
+ 
+  y=25;
+}
+  if(y>475){
+    y=475;
+  }
+  if(x<15){
+    x=15;
+  }
+  if(x>485){
+    x=485;
+}
+  
+   if ( corAmarelo ) {
+   fill(255,255,0);
+ }
+ else {
+   fill(255,255,255);
+  }
+  ellipse(460, yq, 5*raioJ, 5*raioJ); //ellipse oponente
+  yq=yq+2;
+  if(yq>height){
+   yq=random(-600,-50); 
+  }
+     fill(255,255,0);
+
+  ellipse(x,y,30,50); //ellipse do jogador
 }
